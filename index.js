0016b032bf22cd724659a8da6c6f0197a17177c3
Thiago <thiagoedusan5.11@outlook.com>
2023-06-27 19:18:08 -0300

    ...

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const petsRoute = require('./routes/petsRoute');
-const errorHandler = require('./middlewares/errorHandler');
+//const petsRoute = require('./routes/petsRoute');
+//const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.get('/', (_req, res) => res.status(200).send('API rodando'));
 //app.use('/pets-handler', petsRoute);
-app.use(errorHandler);
+//app.use(errorHandler);
 
 const PORT = 3001;
 app.listen(PORT, () => console.log('online na porta: ', PORT));
