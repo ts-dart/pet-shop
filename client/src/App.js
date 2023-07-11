f09bc633df619de5f1edcfb4c24c67cb5acd7605
Thiago Henrique <68298881+ts-dart@users.noreply.github.com>
2023-07-11 12:18:24 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import './styles/View.css';
 function App() {
   return (
     <>
+      {alert('A instancia permanece inativa quando não esta sendo usada, isso pode causar atraso no carregamento das informações.')}
       <Header/>
       <main>
         <Container /> 
